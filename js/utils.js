c000ed4ef9bff332d6b2bff16e9d41b834eb32d5
Dmitry Novikov <Dmitriy.Novikov@waveaccess.global>
2022-03-31 15:41:51 +0500

    module11-task1: работа с серверным api;

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,7 @@
 import {isNonNegativeInt} from './validation.js';
 
+const ALERT_SHOW_TIME = 5000;
+
 function getRandomInt(min, max) {
   if (!isNonNegativeInt(min) || !isNonNegativeInt(max)) {
     throw new Error('Аргументы должны быть неотрицательными целыми числами');
@@ -14,4 +16,23 @@ const isEscapeKey = (evt) => (
   evt.key === 'Escape'
 );
 
-export {getRandomInt, isEscapeKey};
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+  alertContainer.textContent = message;
+  document.body.append(alertContainer);
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export {getRandomInt, isEscapeKey, showAlert};
